3779256c1f8620d15e377599ad509816957c6f0d
defycab <love.bellinder@gmail.com>
2024-09-04 15:59:38 +0200

    add greeting type. Test clear

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,7 +1,13 @@
 import { deepEqual } from "node:assert/strict";
 import { test } from "node:test";
 
-function greet() {}
+type greeting = {
+  message: string
+}
+
+function greet(name: string) :greeting {
+  return { message: `Welcome, ${name}!`}
+}
 
 test("should greet person", () => {
   const result = greet("Alek");
